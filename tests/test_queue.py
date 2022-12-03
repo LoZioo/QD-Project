2b05d4e28912663609cd5e10f624a7eab439cd19
LoZioo <60042676+LoZioo@users.noreply.github.com>
2022-12-03 14:17:03 +0100

    feat: added content check and comments.

--- tests/test_queue.py
+++ tests/test_queue.py
@@ -5,19 +5,29 @@ ATTEMPTS = 3
 def test_Queue() -> None:
 	q = Queue[int]()
 
+	# ---| isEmpty and len |---
 	assert q.isEmpty()
 	assert len(q) == 0
 
+	# ---| enque |---
 	for i in range(ATTEMPTS):
 		q.enque(i)
 
+	# ---| isEmpty and len |---
 	assert not q.isEmpty()
 	assert len(q) == ATTEMPTS
 
+	# ---| content check |---
+	for i in range(ATTEMPTS):
+		assert q[i] == i
+
+	# ---| deque |---
 	for i in range(ATTEMPTS):
 		assert i == q.deque()
 
+	# ---| empty deque |---
 	assert q.deque() == None
 
+	# ---| isEmpty and len |---
 	assert q.isEmpty()
 	assert len(q) == 0
--- tests/test_stack.py
+++ tests/test_stack.py
@@ -5,19 +5,29 @@ ATTEMPTS = 3
 def test_Stack() -> None:
 	s = Stack[int]()
 
+	# ---| isEmpty and len |---
 	assert s.isEmpty()
 	assert len(s) == 0
 
+	# ---| push |---
 	for i in range(ATTEMPTS):
 		s.push(i)
 
+	# ---| isEmpty and len |---
 	assert not s.isEmpty()
 	assert len(s) == ATTEMPTS
 
+	# ---| content check |---
+	for i in range(ATTEMPTS):
+		assert s[i] == i
+
+	# ---| pop |---
 	for i in range(ATTEMPTS):
 		assert ATTEMPTS - i - 1 == s.pop()
 
+	# ---| empty pop |---
 	assert s.pop() == None
 
+	# ---| isEmpty and len |---
 	assert s.isEmpty()
 	assert len(s) == 0
