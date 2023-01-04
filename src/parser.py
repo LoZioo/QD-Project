367780a11f44e2699f3c50b6de245b4307348b1c
PennisiVinnykMykyta <sayron.arnorius@yahoo.com>
2023-01-04 16:45:48 +0100

    Feat: Trovato un modo per trovare tutti gli id e i mainText

--- src/parser.py
+++ src/parser.py
@@ -3,6 +3,8 @@ import xml.etree.ElementTree as ET
 mytree = ET.parse('Esempio di delta.graphml')
 myroot = mytree.getroot()
 
-for i in myroot:
-    posX = myroot.find('./graph').find('./node').attrib['positionX']
-    posY = myroot.find('./graph').find('./node').attrib['positionY']
+for item in myroot.findall('.//graph/node'): # Il ciclo per trovare tutti gli id presenti in tutti i nodi
+    idList = item.attrib['id']
+
+for item in myroot.findall('.//graph/node'): # Il ciclo per trovare tutti i mainText presenti in tutti i nodi
+    textList = item.attrib['mainText']
