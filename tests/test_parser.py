62eaf82a12c29fb77c6c77c912d59d775e5e605b
LoZioo <60042676+LoZioo@users.noreply.github.com>
2023-01-05 15:03:47 +0100

    fix: refactor code style.

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,7 +1,13 @@
 from src.parser import Parser
 
-def test_Parser() ->None:
-  parser = Parser("docs/Esempio di delta.graphml")
-  print (parser.get_info_from_nodes())
-  print (parser.get_info_from_edges())
-  print (parser.getPositions())
+def test_Parser() -> None:
+	parser = Parser("docs/Esempio di delta.graphml")
+
+	print("\n\nget_info_from_nodes")
+	print(parser.get_info_from_nodes())
+
+	print("\nget_info_from_edges")
+	print(parser.get_info_from_edges())
+
+	print("\ngetPositions")
+	print(parser.getPositions())
