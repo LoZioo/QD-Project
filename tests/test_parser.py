0f0f55d8a0030d52021a184c96d97f5912b6d974
Federica Di Dio <uni390005@studium.unict.it>
2023-01-11 11:20:47 +0100

    feat: added unit test for getInfoFromNodes

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,12 +1,26 @@
 from src.parser import Parser
+from include.data_test_ASFD import ASFD_init, DirectGraph_init
+from src.parser import Node_t, Edge_t
+
+import numpy as np
 
 def test_Parser() -> None:
 	parser = Parser("docs/Esempio di delta.graphml")
 
-	print("\n\ngetInfoFromNodes:")
-	for node in parser.getInfoFromNodes():
-		print(node)
+	# test for getInfoFromNodes
 
+	firstNode : Node_t = {'posX': 321, 'posY': 147, 'node_id': 0, 'label': '_q1'}
+	secondNode : Node_t = {'posX': 464, 'posY': 63, 'node_id': 1, 'label': 'q2'}
+	thirdNode : Node_t = {'posX': 605, 'posY': 139, 'node_id': 2, 'label': 'q3_'}
+	fourthNode : Node_t = {'posX': 465, 'posY': 220, 'node_id': 3, 'label': 'q4'}
+
+	nodes: list[Node_t] = [firstNode, secondNode, thirdNode, fourthNode]
+
+	nodesParser: list[Node_t] = parser.getInfoFromNodes()
+
+	assert np.array_equal(nodes, nodesParser)
+
+'''
 	print("\ngetInfoFromEdges:")
 	for edge in parser.getInfoFromEdges():
 		print(edge)
@@ -26,3 +40,4 @@ def test_Parser() -> None:
 	
 	print("\ngetDelta")
 	print(parser.getDelta())
+'''
