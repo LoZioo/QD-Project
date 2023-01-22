2702781fa63cae1824947e5f528cbdbbc9dbbab7
Lucia Miriam Cavallaro <luciamiriamcavallaro@gmail.com>
2023-01-22 16:55:28 +0100

    feat: Added test for getPositions and changed assert function for toCouple

--- tests/test_DirectGraphPrinter.py
+++ tests/test_DirectGraphPrinter.py
@@ -10,4 +10,10 @@ def test_DirectGraphPrinter() -> None:
     couple_list : list[(str,str)] = [('q1', 'q2'), ('q1', 'q4'), ('q2', 'q2'), ('q2', 'q3'), ('q3', 'q3'), ('q3', 'q4'), ('q4', 'q4')]
     extracted_list: list[(str,str)] = printerDG.toCouple(parser)
 
-    assert np.array_equal(couple_list,extracted_list)
+    np.testing.assert_equal(couple_list,extracted_list)
+
+    #test getPositions
+    position_dict : dict[str, tuple[int,int]] = {'q1': (321, 147), 'q2': (464, 63), 'q3': (605, 139), 'q4': (465, 220)}
+    extracted_dict : dict[str, tuple[int,int]] = printerDG.getPositions(parser)
+    
+    np.testing.assert_equal(position_dict, extracted_dict)
