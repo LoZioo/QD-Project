4110dcf346491d9a1d7cec2137c2445cdf937f25
LoZioo <60042676+LoZioo@users.noreply.github.com>
2023-01-24 15:18:33 +0100

    chore: style corrections.

--- src/DirectGraph_printer.py
+++ src/DirectGraph_printer.py
@@ -4,20 +4,18 @@ import matplotlib.pyplot as plt
 from src.parser import Parser
 
 class DirectGraphPrinter:
+	graph_to_print: nx.DiGraph							# Graph that this class will print.
+	positions: dict[str, tuple[int, int]]		# The positions where each node will be printed.
 
-	graph_to_print: nx.DiGraph	#Graph that this class will print
-	positions: dict[str, tuple[int, int]]	#The positions where each node will be printed
-
-	#Inizialize graph_to_print and positions
+	# Inizialize graph_to_print and positions.
 	def __init__(self, p: Parser) -> None:
-
 		l_adj = self.toCouple(p)
 		self.graph_to_print = nx.DiGraph()
 		self.graph_to_print.add_nodes_from(p.getLabelArray())
 		self.graph_to_print.add_edges_from(l_adj)
 		self.positions = self.getPositions(p)
 
-	#Using the adjacency matrix to generate the edges of the couples
+	# Using the adjacency matrix to generate the edges of the couples.
 	def toCouple(self, p:Parser) -> list[(str,str)]:
 		adj_matr = p.getAdjMatr()
 		label_arr = p.getLabelArray()
@@ -28,9 +26,8 @@ class DirectGraphPrinter:
 					l = l + [(label_arr[i],label_arr[j])]
 		return l
 
-	#Extract the positions of the nodes
-	def getPositions(self, p:Parser) -> dict[str, tuple[int, int]] :
-
+	# Extract the positions of the nodes.
+	def getPositions(self, p:Parser) -> dict[str, tuple[int, int]]:
 		d = {}
 		nodes = p.getInfoFromNodes()
 
@@ -47,10 +44,9 @@ class DirectGraphPrinter:
 
 		return d
 
-	#Print an image of the graph using the functions of the library Network.x
-	#By passing a path through the parameter "path" the image will be saved in the desired location
+	# Print an image of the graph using the functions of the library Network.x by passing
+	# a path through the parameter "path" the image will be saved in the desired location.
 	def printGraph(self, path: str = "") -> None:
-
 		plt.figure(figsize=(15,10))
 		nx.draw_networkx(self.graph_to_print, self.positions,node_size= 1000,font_size = 18, node_color= 'white', edgecolors= 'black', linewidths= 2, width= 2)
 
